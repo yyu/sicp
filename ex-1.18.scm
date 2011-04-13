1d8e10858ab2c39e74a1f00b8dda7bdc7a785f73
Forrest Y. Yu <forrest.yu@gmail.com>
2011-04-13 23:09:54 +0800

    SICP Exercise 1.18
    --HG--
    branch : src

---
+++ ex-1.18.scm
@@ -0,0 +1,35 @@
+(define (f a b)
+  (define (double n)
+    (+ n n))
+  (define (halve n)
+    (/ n 2))
+  (define (iter x a b)
+    (cond ((= b 0) 0)
+	  ((= b 1) (+ x a))
+	  ((even? b) (iter x (double a) (halve b)))
+	  (else (iter (+ x a) a (+ -1 b)))))
+  (iter 0 a b))
+
+(display "(f 3 0): ")
+(display  (f 3 0))
+(newline)
+
+(display "(f 3 1): ")
+(display  (f 3 1))
+(newline)
+
+(display "(f 3 2): ")
+(display  (f 3 2))
+(newline)
+
+(display "(f 3 3): ")
+(display  (f 3 3))
+(newline)
+
+(display "(f 3 4): ")
+(display  (f 3 4))
+(newline)
+
+(display "(f 3 5): ")
+(display  (f 3 5))
+(newline)
